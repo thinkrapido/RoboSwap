34257589f43a39894751863335909dc5ab07b818
Romeo Disca <romeo.disca@gmail.com>
2023-01-21 22:47:13 +0100

    added powered by

--- src/lib/widgets/header/HeaderWidget.svelte
+++ src/lib/widgets/header/HeaderWidget.svelte
@@ -1,11 +1,14 @@
 <script>
-	import WalletWidget from "../wallet/WalletWidget.svelte";
+    import WalletWidget from "$lib/widgets/wallet/WalletWidget.svelte";
+	import ExternalLink from "../navi/ExternalLink.svelte";
 
 </script>
+
 <div class="flex">
-    <h1 class="text-9xl font-bold underline">
+    <h1 class="text-9xl font-bold underline text-purple-500 text-shadow-header">
         RoboSwap
     </h1>
+    <ExternalLink href="https://robohash.org/">powered by RoboHash</ExternalLink>
     <div class="grow"></div>
     <WalletWidget />    
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      textShadow: {
+        header: '0 0 3px rgba(255, 255, 255, .8), 0 0 15px rgba(255, 255, 100, .9)',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-textshadow')
+  ],
 }
