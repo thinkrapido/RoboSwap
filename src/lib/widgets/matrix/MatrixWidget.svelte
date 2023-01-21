5082cfe52325ecbff671507a171632cb74388a26
Romeo Disca <romeo.disca@gmail.com>
2023-01-21 22:39:49 +0100

    start page is now randomly generating robos

--- src/lib/widgets/matrix/MatrixWidget.svelte
+++ src/lib/widgets/matrix/MatrixWidget.svelte
@@ -4,14 +4,14 @@
     import * as _ from "lodash"
 	import PictureWidget from "../picture/PictureWidget.svelte";
 
-    let range: number[] = _.range(25)
+    let range: number[] = _.range(1, 26)
 
 </script>
 
 <div class="flex flex-wrap mx-auto w-[800px] my-12">
         {#each range as idx}
             <div class="p-4">
-                <PictureWidget />
+                <PictureWidget index={idx} />
             </div>
         {/each }
 </div>
--- src/lib/widgets/picture/PictureWidget.svelte
+++ src/lib/widgets/picture/PictureWidget.svelte
@@ -3,13 +3,20 @@
     import bs58 from "bs58"
     import _ from "lodash"
     import hex from "convert-hex"
+    import { clamp } from "$lib/utils/math"
 
     export let pubkey: string = '5ZLaVaVJdvdqGmvnS4jYgJ7k54Kdev7f1q5LDytjwqJ6'
+    export let index: number = 0
 
     let hash: string = '2a02:8070:c189:220:4dc6:288d:fd8e:9c79'
 
     $: {
-        hash = _.take(bs58.decode(pubkey), 16)
+        index = clamp(Math.floor(+index), 0, 25)
+    }
+    $: {
+        let arr: number[] = Array.from(bs58.decode(pubkey))
+        arr = [index, ...arr]
+        hash = _.take(arr, 16)
         hash = hex.bytesToHex(hash)
         hash = _.chunk(hash, 4).map((d: number[]) => d.join('')).join(':')
     }
